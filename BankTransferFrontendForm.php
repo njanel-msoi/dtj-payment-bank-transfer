6ff9bceb3d4c72d4c14656cc5d7d2652e4f305f0
njanel-msoi <n.janel@msoi.re>
2024-06-25 11:17:53 +0400

    bank transfer payment form

--- BankTransferFrontendForm.php
+++ BankTransferFrontendForm.php
@@ -4,12 +4,19 @@ class BankTransferFrontendForm  extends Daq_Form_Abstract
 {
     public function init()
     {
-        $this->addGroup("default");
+        // this code is taken from the Stripe plugin
+        $amount = getPaymentAmountFromPaymentFormDefault();
+        $notice = "Pour valider un achat par virement, veuillez effectuer un règlement de <b>$amount €</b> sur le compte bancaire qui vous sera transmis à la page de confirmation.<br>
+        <br>N'oubliez pas de valider la commande avec le bouton ci-dessous";
+
+        $this->addGroup("notice");
 
         $e = $this->create("info", "label");
-        $e->setDescription(__("Bank transfer info notice", "wpjobboard"));
-        $e->addClass('wpjb-flash-info banktransfer-info-notice mt-0 ml-0 d-block');
-        $this->addElement($e, "default");
+        $e->setDescription($notice);
+        $e->addClass('banktransfer-info-notice pt-25 mt-0 ml-0 d-block color-brand-2');
+        $this->addElement($e, "notice");
+
+        $this->addGroup("default");
 
         $e = $this->create("fullname");
         $e->setLabel(__("Full Name", "wpjobboard"));
